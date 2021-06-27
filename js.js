522d24811584b78b526ac0c75fc8b2ed7fd7c5ae
Joseph <josephecano@gmail.com>
2021-06-26 20:06:52 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
 var div = document.getElementById('art');
 for (var i = 1; i < 28; i++) {
-    div.innerHTML += '<img src="/content/Untitled_Artwork ' + i + '.jpg"></img>';
+    div.innerHTML += '<img src="' + i + '.jpg"></img>';
 }
